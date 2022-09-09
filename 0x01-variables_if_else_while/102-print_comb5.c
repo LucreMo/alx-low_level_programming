095346cca3135ebc77f313280b06dcae88756cb6
Lucretia <lucretiamotswakhumo@gmail.com>
2022-09-09 16:39:17 -0700

    T12. Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,25 +11,25 @@ int main(void)
 
 	for (i = 0; i < 100; i++)
 	{
-		for (p = 0; p < 100; j++)
+		for (p = i + 1; p < 100; p++)
 		{
-			if (p > i)
+			putchar((i / 10) + '0');
+			putchar((i % 10) + '0');
+			putchar(' ');
+			putchar((p / 10) + '0');
+			putchar((p % 10) + '0');
+			
+			if (i == 98 && p == 99)
 			{
-				putchar((i / 10) + '0');
-				putchar((i % 10) + '0');
-				putchar(' ');
-				putchar((p / 10) + '0');
-				putchar((p % 10) + '0');
-				if (i != 98)
-				{
-					putchar(',');
-					putchar(' ');
-				}
+				break;
 			}
+			
+			putchar(',');
+			putchar(' ');
 		}
 	}
-
+	
 	putchar('\n');
-
+	
 	return (0);
 }
