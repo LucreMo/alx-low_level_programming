9c9b65b29dadacfc04ac6814c78a646987d29916
Lucretia <lucretiamotswakhumo@gmail.com>
2022-09-08 18:27:15 -0700

    T6. Size is not grandeur, and territory does not make a nation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,6 +4,7 @@
 * main - Prints size of many var types
 *
 * Description: using the main function
+* this program prints the sizes of many var types
 * return: 0 
 */
 int main(void)
@@ -14,10 +15,10 @@ int main(void)
 	long long lli;
 	float f;
 
-	printf("Size of a char: %i byte(s)\n", sizeof(c));
-	printf("Size of an int: %i byte(s)\n", sizeof(i));
-	printf("Size of a long int: %i byte(s)\n", sizeof(li));
-	printf("Size of a long long int: %i byte(s)\n", sizeof(lli));
-	printf("Size of a float: %i byte(s)\n", sizeof(f));
+	printf("Size of a char: %d byte(s)\n", sizeof(c));
+	printf("Size of an int: %d byte(s)\n", sizeof(i));
+	printf("Size of a long int: %d byte(s)\n", sizeof(li));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(lli));
+	printf("Size of a float: %d byte(s)\n", sizeof(f));
 	return (0);
 }
