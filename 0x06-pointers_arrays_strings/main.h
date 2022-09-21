e8b40aaba339aa2cc3a10d8ab42e028df27b3559
Lucretia <lucretiamotswakhumo@gmail.com>
2022-09-21 10:52:01 -0700

    T0. strcat

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -3,18 +3,15 @@
 #include <stdio.h>
 
 /**
- * reverse_array - Reverses the content of an array of integer
- * Header file containing prototypes for all functions
- * used in the 0x06. C - More pointers, arrays and strings directory
- * @a: first parm
- * @n: second param
+ * void prototypes()
+ * int prototypes()
+ * char prototypes()
  */
 
 void reverse_array(int *a, int n);
 void print_number(int n);
-void print_buffer(char *b, int size;
+void print_buffer(char *b, int size);
 int _strcmp(char *s1, char *s2);
-int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -23,5 +20,6 @@ char *cap_string(char *);
 char *leet(char *);
 char *rot13(char *);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
+char *add_strings(char *n1, char *n2, char *r, int r_index);
 
 #endif /*MAIN_H*/
