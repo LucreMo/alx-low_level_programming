f8c91b7ca3953ef12fbea1635742f5f120463ac3
Lucretia <lucretiamotswakhumo@gmail.com>
2022-09-21 09:35:12 -0700

    fix

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,17 +1,18 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdio.h>
 
 /**
- * void prototypes()
- * int prototypes()
- * char prototypes()
+ * File: main.h
+ * Auth: LM
+ * Desc: Header file containing prototypes for all functions
+ * used in the 0x06. C - More pointers, arrays and strings directory
  */
 
 void reverse_array(int *a, int n);
 void print_number(int n);
 void print_buffer(char *b, int size;
 int _strcmp(char *s1, char *s2);
+int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
